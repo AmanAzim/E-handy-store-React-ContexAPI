81409a2312bca58a4e73ead340ef2a8ccf4cf6f9
Aman <rumman.djuice1@gmail.com>
2019-05-04 19:43:32 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {Switch, Route} from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; //for bootstrap
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import PageNotFound from './components/PageNotFound';
+
 
 function App() {
   return (
       <React.Fragment>
-          <h1>Hello Aman</h1>
+         <Navbar/> {/*It is not included inside the <Switch> because we want to show the navbar in every rendered page*/}
+
+          <Switch>
+              <Route path="/" exact component={ProductList}/>
+              <Route path="/details" component={Details}/>
+              <Route path="/cart" component={Cart}/>
+
+              <Route component={PageNotFound}/> {/*To catch all the unknown route*/}
+          </Switch>
+
       </React.Fragment>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {NavLink} from "react-router-dom";
+
+export default class Navbar extends Component {
+    render() {
+        return (
+          <div>
+              <h3>Hello from Navbar</h3>
+          </div>
+        );
+    }
+}
+
+
