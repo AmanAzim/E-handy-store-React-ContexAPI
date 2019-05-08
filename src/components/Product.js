201dd3c2afb8af034177fd19bd83d1f2fca37f30
Aman <rumman.djuice1@gmail.com>
2019-05-08 17:11:13 +0200

    commit

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,24 @@ import {ProductContext} from '../context';
 import {NavLink} from 'react-router-dom';
 
 const ProductWrapper=styled.div`
-    
+    .card {
+        border-color:transparent;
+        transition:all 1s linear;
+    }
+    .card-footer {
+        background:transparent;
+        border-top:transparent;
+        transition:all 1s linear;
+    }
+    &:hover {
+        .card {
+            border: 0.4rem solid rgba(0,0,0,0,0.2);
+            box-shadow:2px 2px 5px 0px rgba(0,0,0,0.2);
+        }
+        .card-footer {
+            background:rgba(247,247,247);
+        }
+    }
 `;
 
 class Product extends Component {
@@ -16,12 +33,22 @@ class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container py-5" onClick={console.log('Clicked')}>
+
+                    <div className="img-container py-5" onClick={()=>console.log('Clicked')}>
+
                         <NavLink to="/details"><img src={img} alt="product" className="card-img-top"/></NavLink>
+
+                        <button className="cart-btn" disabled={inCart} onClick={()=>console.log('add to cart')}>
+                            {inCart? (<p className="mb-0">In Cart</p>):(<i className="fas fa-cart-plus"/>)}
+                        </button>
+
+                    </div>
+
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">{title}</p>
+                        <h5 className="text-red mb-0"><span className="mr-1">$</span>{price}</h5>
                     </div>
                 </div>
-
-
             </ProductWrapper>
         );
     }
